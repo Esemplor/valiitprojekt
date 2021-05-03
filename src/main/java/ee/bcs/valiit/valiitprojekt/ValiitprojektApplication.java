a6330b7284128e7f60c152df0242bdc9fc8ff7c0
Arvo <arvo.sarapuu@atko.ee>
2021-05-03 14:03:18 +0300

    Initial commit

---
+++ src/main/java/ee/bcs/valiit/valiitprojekt/ValiitprojektApplication.java
@@ -0,0 +1,13 @@
+package ee.bcs.valiit.valiitprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValiitprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValiitprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/bcs/valiit/valiitprojekt/ValiitprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bcs.valiit.valiitprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValiitprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
